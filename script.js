370d599fb2af1d24015187684572ce53bc882081
KonstaKozh <konstakozh@yandex.ru>
2022-10-21 12:01:01 +0400

    fix script

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-
 const cardTimes = document.querySelectorAll('.card__times')
 
 cardTimes.forEach(function (item, i) {
     const numberTimes = item.querySelectorAll('.card__time').length
     const cardTime = item.querySelectorAll('.card__time')
     if (numberTimes > 4) {
-        cardTime[2].insertAdjacentHTML('afterend','<button class="card__time" onclick="showTime()">ещё...</button>')
-        // console.log('cardTime[2]',cardTime[2])
+        cardTime[2].insertAdjacentHTML('afterend','<button class="card__time" >ещё...</button>')
         cardTime.forEach(function (time, n){
-            // console.log(time)
             if (n > 2) {
-                time.classList.add('hidden__time')
+                time.classList.add('hidden__time', 'add_time')
             }
         })
     }
 })
-function showTime() {
-    const hiddenTime = document.querySelectorAll('.hidden__time')
-    console.log(hiddenTime)
 
-    // if (hiddenTime.length === 0) {
-    //     hiddenTime.forEach(function (item,i) {
-    //         item.className = 'hidden__time'
-    //     })
-    // } else {
-    //     hiddenTime.forEach(function (item, i) {
-    //         item.className = 'card__time'
-    //     })
-    // }
+const cards = document.querySelectorAll('.card')
+cards.forEach(function (card, i) {
+    card.addEventListener("click", showTime)
+})
+
+function showTime(event) {
+    const additionallyTimes = event.currentTarget.querySelectorAll('.add_time')
+    additionallyTimes.forEach(function (item) {
+        item.classList.toggle('hidden__time');
+    })
 }
